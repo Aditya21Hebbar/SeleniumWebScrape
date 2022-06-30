de5b5ff45e620c546387ce1e4e55742d7fa5dfa4
ADITYA-HEBBAR <84819735+Aditya21Hebbar@users.noreply.github.com>
2022-06-30 20:34:06 +0000

    updated project by removing static and moving it to scratch, starting with selenium for dynamic

--- scratch.py
+++ scratch.py
@@ -1,4 +1,9 @@
+import requests
 
 grab_url='https://food.grab.com/sg/en/restaurants'
-
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+response = requests.get(grab_url,headers=headers)
+
+print('status code',response.status_code)
+
