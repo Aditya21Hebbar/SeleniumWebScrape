dc994e692c707db50606371a1f81a4ea53999651
ADITYA-HEBBAR <84819735+Aditya21Hebbar@users.noreply.github.com>
2022-06-30 15:24:51 +0000

    added code

--- scraper.py
+++ scraper.py
@@ -1,1 +1,8 @@
-print('hello world')
+import requests
+
+grab_url='https://www.swiggy.com/restaurants'
+
+response = requests.get(grab_url)
+
+print('status code',response.status_code)
+print('Output', response.text[:1000])
