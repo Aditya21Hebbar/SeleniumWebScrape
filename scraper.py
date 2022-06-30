ede744d7ed8aa6d4693d4d5c5702945a907a62f2
ADITYA-HEBBAR <84819735+Aditya21Hebbar@users.noreply.github.com>
2022-06-30 18:35:19 +0000

    sucssful code

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 import requests
 
-grab_url='https://www.swiggy.com/restaurants'
-
-response = requests.get(grab_url)
+grab_url='https://food.grab.com/sg/en/restaurants'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+response = requests.get(grab_url,headers=headers)
 
 print('status code',response.status_code)
-print('Output', response.text[:1000])
+print('Output',response.text[:100])
