6c44302ab13d0c0f7db3f2858893483fddca923d
ADITYA-HEBBAR <84819735+Aditya21Hebbar@users.noreply.github.com>
2022-07-01 04:44:49 +0000

    Updated scraper.py, sucfully added chromium

--- scraper.py
+++ scraper.py
@@ -1,4 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 grab_url='https://food.grab.com/sg/en/restaurants'
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+def get_driver():
+  chrome_options = Options()
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--headers')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  driver = webdriver.Chrome(options=chrome_options)
+  return driver
+
+if __name__ == "__main__":
+  print("creating driver")
+  driver = get_driver()
+
+  print('fetching the page')
+  driver.get(grab_url)
+  
+  print('page title', driver.title)
